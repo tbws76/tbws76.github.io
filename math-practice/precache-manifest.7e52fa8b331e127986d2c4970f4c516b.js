76406e5be946a0f889ef906611df6ddc7fa58a9d
tbws76 <48369895+tbws76@users.noreply.github.com>
2019-05-11 20:39:57 -0600

    initial commit

---
+++ math-practice/precache-manifest.7e52fa8b331e127986d2c4970f4c516b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d63b2f53bd0f1656483",
+    "url": "/math-practice/static/js/runtime~main.2d63b2f5.js"
+  },
+  {
+    "revision": "1a6a2a03169ce64476c8",
+    "url": "/math-practice/static/js/main.1a6a2a03.chunk.js"
+  },
+  {
+    "revision": "c7ecafe6e14c72cfe94f",
+    "url": "/math-practice/static/js/2.c7ecafe6.chunk.js"
+  },
+  {
+    "revision": "1a6a2a03169ce64476c8",
+    "url": "/math-practice/static/css/main.e8d36c70.chunk.css"
+  },
+  {
+    "revision": "c244d81d6531ce533a0c53f65d1acd9b",
+    "url": "/math-practice/index.html"
+  }
+];
