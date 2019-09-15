be237a90e34514ff7ae9fb02ff3cd70080312dbf
tbws76 <48369895+tbws76@users.noreply.github.com>
2019-09-14 21:59:36 -0600

    Updates

---
+++ flashcards/precache-manifest.e76971f971c93c83a928e2dc182fa2d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7486bc97f59629413d417eeba0bac082",
+    "url": "/flashcards/index.html"
+  },
+  {
+    "revision": "aff4447eb5a28a941f44",
+    "url": "/flashcards/static/css/main.e7c7673d.chunk.css"
+  },
+  {
+    "revision": "889175b920790f17bfcb",
+    "url": "/flashcards/static/js/2.aedda2b7.chunk.js"
+  },
+  {
+    "revision": "aff4447eb5a28a941f44",
+    "url": "/flashcards/static/js/main.4b815d09.chunk.js"
+  },
+  {
+    "revision": "03b93c7181276e37e9b0",
+    "url": "/flashcards/static/js/runtime~main.f45cdabc.js"
+  }
+]);
